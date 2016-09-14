acd1e95f4303bcd6cf32f44b301b1154371bca08
Igor Ferreira <ipferreira@inf.ufrgs.br>
2016-09-14 14:03:55 -0300

    Adicionei encoding UTF-8 pra não dar problemas com acentos e adicionei gitignore

--- AI/Controller.py
+++ AI/Controller.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from .State import State
 from random import randint
 from parser import parse_weights
--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from sys import argv
 from Simulator.simulator import game
 
